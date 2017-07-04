6955b64cbe34a802208fea441d73a0b8c40121cd
windtalkerz <windtalkerz@me.com>
2017-07-04 20:15:52 +0200

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-Super_Example/Pods-Super_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Super_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Super_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-Super_Tests/Pods-Super_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Super_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Super_Tests
+@end
