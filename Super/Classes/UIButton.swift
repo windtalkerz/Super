7b4ff79ee4c9a34a714df56633bbf1df92cfef93
windtalkerz <windtalkerz@me.com>
2017-07-04 20:35:51 +0200

    Added test class

---
+++ Super/Classes/UIButton.swift
@@ -0,0 +1,15 @@
+//
+//  UIButton.swift
+//  Pods
+//
+//  Created by Marc Orlieb on 04.07.17.
+//
+//
+
+import UIKit
+
+
+public extension UIButton {
+    
+    public func tap() { print("Super.extension UIButton.tap") }
+}
